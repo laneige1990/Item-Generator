df1a034bdd32c2d81cb48b89c4af0a184f77586c
laneige1990 <glane@hotmail.co.uk>
2020-05-08 00:57:01 +0100

    Add domain handler

--- loader.php
+++ loader.php
@@ -5,6 +5,13 @@ define('SERVER', 'localhost');
 define('DATABASE', 'item-tool');
 define('USER', 'root');
 define('PASSWORD', '');
+echo "<script>console.log(".json_encode($_SERVER).");</script>";
+if ($_SERVER['HTTP_HOST'] == "itemtool.local"){
+    define('IMG_PATH', '/assets/images');
+}else{
+    define('IMG_PATH', '/public_html/assets/images');
+}
+
 
 // load database
 require_once('models/database.php');
