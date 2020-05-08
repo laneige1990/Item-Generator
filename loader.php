06357d5c12b0f82ed8b859b8a11d62aaa3ab5665
laneige1990 <glane@hotmail.co.uk>
2020-05-08 15:06:50 +0100

    Fix database config for multiple domains and add gitignore

--- loader.php
+++ loader.php
@@ -1,16 +1,16 @@
 <?php
 
 // Globals
-define('SERVER', 'localhost');
-define('DATABASE', 'item-tool');
-define('USER', 'root');
-define('PASSWORD', '');
-
 if ($_SERVER['HTTP_HOST'] != "itemtool.local"){
     define('SERVER', '10.169.0.210');
     define('DATABASE', 'fastlane1_qtg7');
     define('USER', 'root');
     define('PASSWORD', 'cx7y%a29FI%U3#5L');
+}else{
+    define('SERVER', 'localhost');
+    define('DATABASE', 'item-tool');
+    define('USER', 'root');
+    define('PASSWORD', '');
 }
 
 // load database
