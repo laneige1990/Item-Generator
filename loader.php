7e2d50965241ead74af9c2f749c7718b5833eb10
laneige1990 <glane@hotmail.co.uk>
2020-05-08 01:11:17 +0100

    Add database detail config for remote site

--- loader.php
+++ loader.php
@@ -6,5 +6,12 @@ define('DATABASE', 'item-tool');
 define('USER', 'root');
 define('PASSWORD', '');
 
+if ($_SERVER['HTTP_HOST'] != "itemtool.local"){
+    define('SERVER', '10.169.0.210');
+    define('DATABASE', 'fastlane1_qtg7');
+    define('USER', 'root');
+    define('PASSWORD', 'cx7y%a29FI%U3#5L');
+}
+
 // load database
 require_once('models/database.php');
